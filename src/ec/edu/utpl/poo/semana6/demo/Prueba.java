b09b498c5345300892ff9a1d46bf92ec0eb9ef82
vecarrion <73541103+syluh@users.noreply.github.com>
2021-05-12 17:20:27 -0500

    Agregue el origen del saludo

--- src/ec/edu/utpl/poo/semana6/demo/Prueba.java
+++ src/ec/edu/utpl/poo/semana6/demo/Prueba.java
@@ -3,5 +3,6 @@ package ec.edu.utpl.poo.semana6.demo;
 public class Prueba {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Desde Loja");
     }
 }
